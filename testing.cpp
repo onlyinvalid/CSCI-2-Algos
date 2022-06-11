a3b1daa1f6dbd691221deef08abcc91ed74d88f3
Alex <pulikkottil_a@yahoo.ca>
2022-06-10 20:56:03 -0700

    making a testing cpp file, updating the readme file

---
+++ testing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class hello
+{
+    struct node 
+   {
+       int value;
+       struct node* next;
+   };
+
+};
+
+int main ()
+{
+
+}
