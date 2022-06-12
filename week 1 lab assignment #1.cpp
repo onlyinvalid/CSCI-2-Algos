f8c6be2791969cd5dcbf8f6544ab9fa5ca79a3ed
Alex <pulikkottil_a@yahoo.ca>
2022-06-12 09:53:46 -0700

    starting week 1 assignment

---
+++ week 1 lab assignment #1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct MovieData
+{
+    string Title;
+    string Director;
+    string YearR;
+    string RunT;
+
+};
+
+int main()
+{
+    
+}
